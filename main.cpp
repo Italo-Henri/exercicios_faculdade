fceca484379c90f00dfe52a218cb18f905268a60
Italo-Henri <82114416+Italo-Henri@users.noreply.github.com>
2021-09-01 23:01:58 -0300

    Meu primeiro código em C++

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//tentativa com exito.
+
+using namespace std;
+
+int main() {
+
+ 	cout << "Olá Mundo!" << endl;
+ 	cout << "Este é o meu primeiro código em C++" << endl;
+	return 0;
+}
